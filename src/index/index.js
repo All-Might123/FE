a08675d8eae571f61d86f01cdab0c18eab263099
namvkim <sonnamttt@gmail.com>
2021-09-23 22:12:23 +0700

    complete

---
+++ src/index/index.js
@@ -0,0 +1,189 @@
+import React from 'react';
+function HomePage(props) {
+    return (
+        <div className="index_container">
+            <div className="rankbox_container">
+                <div className="title">
+                    <table>
+                        <tbody><tr>
+                            <th>Rank</th>
+                            <th>User Name</th>
+                            <th>Level</th>
+                        </tr>
+                        </tbody></table>
+                </div>
+                <div className="content">
+                    <table>
+                        <tbody><tr>
+                            <td><img src="./img/rank1.ico" alt="" /></td>
+                            <td>
+                                <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                            </td>
+                            <td>5</td>
+                        </tr>
+                            <tr>
+                                <td><img src="./img/rank2.ico" alt="" /></td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td><img src="./img/rank3.ico" alt="" /></td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>
+                                    <a href="https://www.w3schools.com/cssref/pr_border-spacing.asp">le son nam</a>
+                                </td>
+                                <td>5</td>
+                            </tr>
+                        </tbody></table>
+                </div>
+            </div>
+            <div className="user_info_container">
+                <div className="user_info">
+                    <img src="https://thuthuatnhanh.com/wp-content/uploads/2018/07/anh-dai-dien-dep.jpg" alt="" />
+                    <div>
+                        <div className="user_name">Le son nam</div>
+                        <div className="user_level">Lv 48</div>
+                        <div className="user_exp" />
+                    </div>
+                </div>
+                <div className="user_row">
+                    <div>Score</div>
+                    <div>120000212</div>
+                </div>
+                <div className="user_row">
+                    <div>Task</div>
+                    <div>1251</div>
+                </div>
+            </div>
+            <div className="progress_bar_container">
+                <div className="progress_team">
+                    <div className="progress_title">
+                        <div className="progress_name">Sell website team</div>
+                        <a href="https://flatuicolors.com/palette/gb"><i className="fas fa-chevron-circle-right" /></a>
+                    </div>
+                    <div className="progress_row">
+                        <div>Leader</div>
+                        <div>Le son nam</div>
+                    </div>
+                    <div className="progress_row">
+                        <div>Score</div>
+                        <div>2400/3200</div>
+                    </div>
+                    <div className="progress_row">
+                        <div>Member</div>
+                        <div>15</div>
+                    </div>
+                    <div className="progress_row">
+                        <div>Task complete</div>
+                        <div>6/15</div>
+                    </div>
+                    <div className="progress_row">
+                        <div>Task unfinished</div>
+                        <div>4/15</div>
+                    </div>
+                    <div className="progress_row">
+                        <div>Task late</div>
+                        <div>5/15</div>
+                    </div>
+                </div>
+            </div>
+            <div className="pet_ui_container">
+                <img src="./img/How-to-draw-an-anime-animal-removebg-preview.png" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
